a7935003a93ca035938aafdafe2f115a58281c87
Kotiuk Sofiia <ipz231_ksv@student.ztu.edu.ua>
2025-01-23 17:02:40 +0200

    Додала TechPraktProject

---
+++ TechPraktProject/Program.cs
@@ -0,0 +1,6 @@
+Console.WriteLine("Enter first number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"The sum is: {num1 + num2}");
+
